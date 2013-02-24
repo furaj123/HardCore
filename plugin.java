4e3dec465080ca45a4128a875e331895e5780024
furaj123 <furaj123@gmail.com>
2013-02-24 12:07:11 +0100

    Create plugin.java

---
+++ plugin.java
@@ -0,0 +1,49 @@
+package me.furaj123.pl;
+
+import java.io.File;
+import java.util.logging.Logger;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class plugin extends JavaPlugin{
+    private static final Logger LOG = Logger.getLogger(plugin.class.getName());
+    protected ListStore banList;
+    
+    public static Logger getLOG() {
+        return LOG;
+    }
+    private Configuration config;
+    
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        LOG.info("Wczytuje sie plugin...");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        LOG.info("Plugin jest wylanczony!");
+        banList.save();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        LOG.info("Plugin jest wlanczony!");
+        
+        banList.load();
+        
+        PluginManager pm = this.getServer().getPluginManager();
+        pm.registerEvents(new PlayerListener(this), this);
+        pm.registerEvents(new PlayerLoginListener(this), this);
+        
+        String pluginFolder = this.getDataFolder().getAbsolutePath();
+        
+        (new File(pluginFolder)).mkdirs();
+        
+        this.banList = new ListStore(new File(pluginFolder + File.separator + "zbanowani-gracze.txt"));
+        
+    }
+}
